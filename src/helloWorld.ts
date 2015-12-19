968f06efc43c988733fe5269f63a49df7ff53fee
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-12-19 11:45:19 +0000

    Playing around

--- src/helloWorld.ts
+++ src/helloWorld.ts
@@ -1,6 +1,41 @@
+interface Person {
+    firstName: string;
+    lastName?: string;
+}
 
-function hello(whatever) {
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = `${firstName} ${middleInitial} ${lastName}`;
+    }
+}
+
+interface StringArray {
+    [index: number]: string;
+}
+
+function hello(whatever: string) {
     document.body.innerHTML = 'Hello ' + whatever;
 }
 
-hello('Simon');
+function helloInterface(person: Person) {
+    document.body.innerHTML = `Hello ${person.firstName} ${person.lastName}`;
+}
+
+function helloClass(student: Student) {
+    document.body.innerHTML = `Hello ${student.fullName}`;
+}
+
+//hello('Simon Adcock');
+//helloInterface({
+//    firstName: 'Simon',
+//    lastName: 'Adcock123'
+//});
+//const stud: Student = new Student('Simon', 'J', 'Adcock');
+//helloClass(stud);
+helloInterface({
+    firstName: 'Simon'
+});
+
+var stringArray: StringArray = ['Simon', 'Adcock'];
+console.log(stringArray[0]);
